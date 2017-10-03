f5d9f7c65d096e5c615a96ca64f889451ccf13c9
amit28manchanda <amit28manchanda@mail.com>
2017-10-03 18:27:14 +0530

    navigation bar

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
    version :thumb do
-    process resize_to_fit: [370, 210]
+    process resize_to_fit: [50,50]
   end
   # Process files as they are uploaded:
   # process scale: [200, 300]
